2148cbfa33020c3f77ec2f5f9711a6d4b0a0ee7c
vistacyy <vistacyy@163.com>
2017-05-23 08:11:21 +0800

    fs

--- fs.js
+++ fs.js
@@ -24,19 +24,63 @@ var fs = require('fs');
 //   });
 // });
 
-fs.readdir('/etc/', function(err, files) {
-  if (err) throw err;
+// fs.readdir('./', function(err, files) {
+//   if (err) throw err;
+//   files.forEach(function(file) {
+//     fs.stat('./' + file, function(err, stats) {
+//       if (err) throw err;
+//       if (stats.isFile()) {
+//         console.log("%s is file", file);
+//       } else if (stats.isDirectory()) {
+//         console.log("%s is a directory", file);
+//       }
+//       console.log('stats:  %s', JSON.stringify(stats));
+//     });
+//   });
+// });
 
-  files.forEach(function(file) {
-    fs.stat('/etc/' + file, function(err, stats) {
-      if (err) throw err;
+// fs.watchFile('./message.txt', function(curr, prev) {
+//   console.log('the current mtime is: ' + curr.mtime);
+//   console.log('the previous mtime was: ' + prev.mtime);
+// });
 
-      if (stats.isFile()) {
-        console.log("%s is file", file);
-      } else if (stats.isDirectory()) {
-        console.log("%s is a directory", file);
-      }
-      console.log('stats:  %s', JSON.stringify(stats));
-    });
+
+// function readLines(input, func) {
+//   var remaining = '';
+//   input.on('data', function(data) {
+//     remaining += data;
+//     var index = remaining.indexOf('\n');
+//     var last = 0;
+//     while (index > -1) {
+//       var line = remaining.substring(last, index);
+//       last = index + 1;
+//       func(line);
+//       index = remaining.indexOf('\n', last);
+//     }
+//     remaining = remaining.substring(last);
+//   });
+//   input.on('end', function() {
+//     if (remaining.length > 0) {
+//       func(remaining);
+//     }
+//   });
+// }
+// function func(data) {
+//   console.log('Line: ' + data);
+// }
+// var input = fs.createReadStream('message.txt');
+// readLines(input, func);
+
+function fileCopy(filename1, filename2, done) {
+  var input = fs.createReadStream(filename1);
+  var output = fs.createWriteStream(filename2);
+
+  input.on('data', function(d) { output.write(d); });
+  input.on('error', function(err) { throw err; });
+  input.on('end', function() {
+    output.end();
+    if (done) done();
   });
-});
+}
+
+fileCopy('./message.txt', './target.txt');
