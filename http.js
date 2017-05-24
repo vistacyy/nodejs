9407d35117ed1c02ff7c62251448ce3f37b986b1
vistacyy <vistacyy@163.com>
2017-05-24 08:11:20 +0800

    net

--- http.js
+++ http.js
@@ -1,36 +1,78 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
+var fs = require('fs');
+var util = require('util');
 
-// http.createServer(function (request, response) {
-//   //   response.writeHead(200, { 'Content-Type': 'text/plain' });
-//   //   response.write("Hello World");
-//   //   response.end();
+// var mimetype = {
+//   'txt': 'text/plain',
+//   'html': 'text/html',
+//   'css': 'text/css',
+//   'xml': 'application/xml',
+//   'json': 'application/json',
+//   'js': 'application/javascript',
+//   'jpg': 'image/jpeg',
+//   'jpeg': 'image/jpeg',
+//   'gif': 'image/gif',
+//   'png': 'image/png',
+//   'svg': 'image/svg+xml'
+// };
+// var page_404 = function(req, res, path) {
+//   res.writeHead(404, {
+//     'Content-Type': 'text/html'
+//   });
+//   res.write('<!doctype html>\n');
+//   res.write('<title>404 Not Found</title>\n');
+//   res.write('<h1>Not Found</h1>');
+//   res.write(
+//     '<p>The requested URL ' +
+//     path +
+//     ' was not found on this server.</p>'
+//   );
+//   res.end();
+// };
+// var page_500 = function(req, res, error) {
+//   res.writeHead(500, {
+//     'Content-Type': 'text/html'
+//   });
+//   res.write('<!doctype html>\n');
+//   res.write('<title>Internal Server Error</title>\n');
+//   res.write('<h1>Internal Server Error</h1>');
+//   res.write('<pre>' + util.inspect(error) + '</pre>');
+// };
 
-//   // 或者
-//   fs.createReadStream(`${__dirname}/resource/index.html`).pipe(response);
-// }).listen(8080, '127.0.0.1');
-// console.log('Server running on port 8080.');
+// http.createServer(function(req, res) {
 
+//   var pathname = url.parse(req.url).pathname;
+//   var realPath = __dirname + "/resource" + pathname;
+//   fs.exists(realPath, function(exists) {
+//     if (!exists) {
+//       return page_404(req, res, pathname);
+//     } else {
+//       var file = fs.createReadStream(realPath);
+//       res.writeHead(200, {
+//         'Content-Type': mimetype[realPath.split('.').pop()] || 'text/plain'
+//       });
+//       file.on('data', res.write.bind(res));
+//       file.on('close', res.end.bind(res));
+//       file.on('error', function(err) {
+//         return page_500(req, res, err);
+//       });
+//     }
+//   });
+// }).listen(8000, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:8000/');
 
-var destinationFile, fileSize, uploadedBytes;
-
-http.createServer(function (request, response) {
-  response.writeHead(200);
-  destinationFile = fs.createWriteStream("destination.md");
-  request.pipe(destinationFile);
-  fileSize = request.headers['content-length'];
-  uploadedBytes = 0;
-
-  request.on('data', function (d) {
-    uploadedBytes += d.length;
-    var p = (uploadedBytes / fileSize) * 100;
-    response.write("Uploading " + parseInt(p, 0) + " %\n");
-  });
-
-  request.on('end', function () {
-    response.end("File Upload Complete");
-  });
-}).listen(3030, function () {
-  console.log("server started");
-});
+// http.get("http://cnodejs.org/", function(res) {
+//   var size = 0;
+//   var chunks = [];
+//   res.on('data', function(chunk) {
+//     size += chunk.length;
+//     chunks.push(chunk);
+//   });
+//   res.on('end', function() {
+//     var data = Buffer.concat(chunks, size);
+//     console.log(data.toString());
+//   });
+// }).on('error', function(e) {
+//   console.log("Got error: " + e.message);
+// });
