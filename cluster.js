c2cb4bf8100e2f465ad4b71cb93e3fa8f19da653
vistacyy <vistacyy@163.com>
2017-06-05 10:27:24 +0800

    c

--- cluster.js
+++ cluster.js
@@ -1,22 +1,30 @@
-var cluster = require('cluster');
-var http = require('http');
+let cluster = require('cluster');
+let http = require('http');
+
 if (cluster.isMaster) {
-  var numWorkers = require('os').cpus().length;
+
+  let numWorkers = require('os').cpus().length;
   console.log('Master cluster setting up ' + numWorkers + ' workers...');
-  for (var i = 0; i < numWorkers; i++) {
+
+  for (let i = 0; i < numWorkers; i++) {
     cluster.fork();
   }
+
   cluster.on('online', function (worker) {
     console.log('Worker ' + worker.process.pid + ' is online');
   });
+
   cluster.on('exit', function (worker, code, signal) {
     console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
     console.log('Starting a new worker');
     cluster.fork();
   });
+
 } else {
+
   http.createServer(function (req, res) {
     res.writeHead(200);
     res.end("hello world\n");
   }).listen(8000);
+  
 }
