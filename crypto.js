229617a9fabd9c0b2c3bd043554925f09ff96257
vistacyy <vistacyy@163.com>
2017-06-09 11:07:25 +0800

    1

--- crypto.js
+++ crypto.js
@@ -20,22 +20,22 @@ var fs = require('fs');
 // console.log('crypted',crypted,'dec',dec);
 
 // 签名验证
-// var data = 'abcdef';
-// var privateKey=fs.readFileSync('./resource/server.pem').toString();
-// var publicKey = fs.readFileSync('./resource/cert.pem').toString();
-// var sign = crypto.createSign('RSA-SHA256');
-// sign.update(data);
-// var signText = sign.sign(privateKey, 'hex');
-// var verify = crypto.createVerify('RSA-SHA256');
-// verify.update(data);
-// var result=verify.verify(publicKey, signText, 'hex');
-// console.log('result',result);
+var data = 'abcdef';
+var privateKey=fs.readFileSync('./resource/server.pem').toString();
+var publicKey = fs.readFileSync('./resource/cert.pem').toString();
+var sign = crypto.createSign('RSA-SHA256');
+sign.update(data);
+var signText = sign.sign(privateKey, 'hex');
+var verify = crypto.createVerify('RSA-SHA256');
+verify.update(data);
+var result=verify.verify(publicKey, signText, 'hex');
+console.log('result',result);
 
 // 迪菲－赫尔曼密钥交换
-var alice = crypto.getDiffieHellman('modp5');
-var bob = crypto.getDiffieHellman('modp5');
-alice.generateKeys();
-bob.generateKeys();
-var alice_secret = alice.computeSecret(bob.getPublicKey(), 'binary', 'hex');
-var bob_secret = bob.computeSecret(alice.getPublicKey(), 'binary', 'hex');
-console.log(alice_secret == bob_secret);
+// var alice = crypto.getDiffieHellman('modp5');
+// var bob = crypto.getDiffieHellman('modp5');
+// alice.generateKeys();
+// bob.generateKeys();
+// var alice_secret = alice.computeSecret(bob.getPublicKey(), 'binary', 'hex');
+// var bob_secret = bob.computeSecret(alice.getPublicKey(), 'binary', 'hex');
+// console.log(alice_secret == bob_secret);
